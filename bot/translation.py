beb70b934c59c7228b5496c7e3d328adf740502c
Revanth JK <83715631+revanth-jk@users.noreply.github.com>
2021-11-19 02:36:03 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!</b>
+
+I Am Just Movie Search Bot....😉
+
+Just Type the name of your Movie you Want..!
+
+For More Details
+@RevanthJK
+"""    
